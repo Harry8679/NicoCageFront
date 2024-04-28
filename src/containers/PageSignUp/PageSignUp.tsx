81884e34d095a7effaa41d3fab119727ab11dbe6
Harry MacCode <emarh.harry.code@gmail.com>
2024-04-28 21:38:56 +0200

    Signup Page

--- src/containers/PageSignUp/PageSignUp.tsx
+++ src/containers/PageSignUp/PageSignUp.tsx
@@ -33,11 +33,11 @@ const PageSignUp: FC<PageSignUpProps> = ({ className = "" }) => {
   return (
     <div className={`nc-PageSignUp  ${className}`} data-nc-id="PageSignUp">
       <Helmet>
-        <title>Sign up || Booking React Template</title>
+        <title>Inscription || Booking React Template</title>
       </Helmet>
       <div className="container mb-24 lg:mb-32">
         <h2 className="my-20 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-neutral-900 dark:text-neutral-100 justify-center">
-          Signup
+          Inscription
         </h2>
         <div className="max-w-md mx-auto space-y-6 ">
           <div className="grid gap-3">
@@ -61,35 +61,39 @@ const PageSignUp: FC<PageSignUpProps> = ({ className = "" }) => {
           {/* OR */}
           <div className="relative text-center">
             <span className="relative z-10 inline-block px-4 font-medium text-sm bg-white dark:text-neutral-400 dark:bg-neutral-900">
-              OR
+              OU
             </span>
             <div className="absolute left-0 w-full top-1/2 transform -translate-y-1/2 border border-neutral-100 dark:border-neutral-800"></div>
           </div>
           {/* FORM */}
           <form className="grid grid-cols-1 gap-6" action="#" method="post">
-            <label className="block">
-              <span className="text-neutral-800 dark:text-neutral-200">
-                Email address
-              </span>
-              <Input
-                type="email"
-                placeholder="example@example.com"
-                className="mt-1"
-              />
+          <label className="block">
+              <span className="text-neutral-800 dark:text-neutral-200">Votre Nom</span>
+              <Input type="email" placeholder="Veuillez renseigner votre nom" className="mt-1" />
             </label>
             <label className="block">
-              <span className="flex justify-between items-center text-neutral-800 dark:text-neutral-200">
-                Password
-              </span>
-              <Input type="password" className="mt-1" />
+              <span className="text-neutral-800 dark:text-neutral-200">Votre Prénom</span>
+              <Input type="email" placeholder="Veuillez renseigner votre prénom" className="mt-1" />
             </label>
-            <ButtonPrimary type="submit">Continue</ButtonPrimary>
+            <label className="block">
+              <span className="text-neutral-800 dark:text-neutral-200">Votre Email</span>
+              <Input type="email" placeholder="example@example.com" className="mt-1" />
+            </label>
+            <label className="block">
+              <span className="flex justify-between items-center text-neutral-800 dark:text-neutral-200">Mot de passe</span>
+              <Input type="password" placeholder="Entrez votre mot de passe" className="mt-1" />
+            </label>
+            <label className="block">
+              <span className="flex justify-between items-center text-neutral-800 dark:text-neutral-200">Confirmation du mot de passe</span>
+              <Input type="password" placeholder="Veuillez confirmer votre mot de passe" className="mt-1" />
+            </label>
+            <ButtonPrimary type="submit">Inscription</ButtonPrimary>
           </form>
 
           {/* ==== */}
           <span className="block text-center text-neutral-700 dark:text-neutral-300">
-            Already have an account? {` `}
-            <Link to="/login">Sign in</Link>
+            Avez-vous déjà un compte? {` `}
+            <Link to="/login">Connectez-vous</Link>
           </span>
         </div>
       </div>
